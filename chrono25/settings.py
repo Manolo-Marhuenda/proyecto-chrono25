7fda2e47bee4192c1cee93697f057a2a7838b68d
Manolo-Marhuenda <manolommq@gmail.com>
2026-08-31 12:20:14 +0200

    cambio datos data base

--- chrono25/settings.py
+++ chrono25/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'chrono25.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
     )
 }
